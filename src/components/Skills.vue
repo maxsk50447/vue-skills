e6c2bf3183bc7c8a422715870f64c6346daa93cc
Patipol Wangjaitham <patipol.wa@ku.th>
2018-05-31 23:38:00 +0700

    Finish v-if condition in vue

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -4,6 +4,9 @@
       <ul>
         <li v-for="(data, index) in skills" :key='index'>{{ index }}. {{ data.skill }}</li>
       </ul>
+
+      <p v-if="skills.length > 1">You have more than one skill</p>
+      <p v-else>You have less than or equal to 1 skill</p>
     </div>
   </div>
 </template>
